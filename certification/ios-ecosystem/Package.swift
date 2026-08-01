816128758d5f5001b72d5d140df7ed9e687003db
davidbalbino <davidwilliambalbino@gmail.com>
2026-08-01 01:14:06 -0300

    fix(ci): certify ActivityKit with its shared model

--- certification/ios-ecosystem/Package.swift
+++ certification/ios-ecosystem/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "PamNativePlugins",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v16_2)],
     products: [.library(name: "PamNativePlugins", targets: ["PamNativePlugins"])],
     dependencies: [
         .package(path: "../../ios"),
